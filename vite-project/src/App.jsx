a7a135b1ab95e97ca7671be1130556d4874f2de8
emilijaMarija <z.mari.emilija1@gmail.com>
2022-11-07 15:56:47 +0200

    Create header component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,32 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Header from './Header/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <Header />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>I am a website</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edited <code>src/App.jsx</code> and saved to test HMR ^^
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ vite-project/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import reactLogo from '../assets/react.svg'
+import './Header.module.css'
+
+export default function Header() {
+    return (
+        <header className="Header">
+            <a href="https://vitejs.dev" target="_blank">
+            <img src="../vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+        </header>
+    )
+}
